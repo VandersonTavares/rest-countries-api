96625330c556441f9a215cbecf98ce87f207cdb3
Vanderson Tavares <vandersontavares32@gmail.com>
2023-08-24 22:00:56 -0300

    details-page update

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,15 +1,22 @@
 import { useParams, Link } from 'react-router-dom';
+import { useState } from "react"
 
 import jsonData from "../../data.json"
 
 export const Details = () => {
     const { countryName } = useParams()
 
-    const country = jsonData.find((country) => country.name === countryName);
+    const [country] = useState(jsonData.find((country) => country.name === countryName))
 
     if (!country) {
         return <div>Country not found.</div>;
     }
+
+    // Filtrar informações dos países vizinhos
+    const neighborCountries = country.borders?.map((borderCountryCode) => {
+        return jsonData.find((country) => country.alpha3Code === borderCountryCode);
+    });
+
 
     return (
         <div className="max-w-6xl mx-auto">
@@ -19,10 +26,10 @@ export const Details = () => {
                 </Link>
             </div>
             <div className="details h-[calc(100vh-110px)] flex items-center">
-                <div className="left flag-img w-1/2">
-                    <img src={country.flags.png} alt="" />
+                <div className="left flag-img w-1/2 border">
+                    <img src={country.flags.png} alt={country.nativeName} />
                 </div>
-                <div className="right infos w-1/2">
+                <div className="right infos w-1/2 border">
                     <h1 className="font-bold text-3xl mb-5">{countryName}</h1>
                     <div className="detailed-infos flex justify-between items-center">
                         <div>
@@ -34,16 +41,30 @@ export const Details = () => {
                         </div>
                         <div>
                             <p className="font-semibold">Top Level Domain: <span className="font-normal">{country.topLevelDomain}</span></p>
-                            <p className="font-semibold">Currencies: <span className="font-normal">Belgie</span></p>
-                            <p className="font-semibold">languages: <span className="font-normal">Belgie</span></p>
+                            <div className='currencies'>
+                                <ul className='flex gap-2'>
+                                    <p className='font-semibold'>Currencies: </p>
+                                    {country.currencies?.map((currency, index) => (
+                                        <li key={currency.code}>{currency.name}{index !== country.currencies.length - 1 && ','}</li>
+                                    ))}
+                                 </ul>
+                            </div>
+                            <div className="languages">
+                                <ul className='flex gap-2'>
+                                    <p className='font-semibold'>Languages:</p>
+                                    {country.languages.map((lang, index) => (
+                                        <li key={lang.name}>{lang.name}{index !== country.languages.length - 1 && ','}</li>
+                                    ))}
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                    <div className="flex mt-10">
+                    <div className="flex mt-10 border">
                         <h1 className="font-semibold">Border Countries: </h1>
-                        <div className="flex gap-4">
-                            <span className="px-2 py-1 text-sm w-20 text-center shadow-sm">France</span>
-                            <span className="px-2 py-1 text-sm w-20 text-center shadow-sm">Germany</span>
-                            <span className="px-2 py-1 text-sm w-20 text-center shadow-sm">Netherlands</span>
+                        <div className='flex flex-wrap gap-2 ml-5'>
+                            {neighborCountries?.map((neighbor) => (
+                                <div key={neighbor?.alpha3Code} className="ftext-sm px-2 py-2 text-center shadow-sm bg-emerald-400 rounded">{neighbor?.name}</div>
+                            ))}
                         </div>
                     </div>
                 </div>
--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import { Details } from './components/Details.tsx'
 import { Home } from './components/Home.tsx'
+import { Navbar } from './components/Navbar.tsx'
 
 import ErrorPage from './components/ErrorPage.tsx'
 
@@ -23,6 +24,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Navbar />
     <RouterProvider router={router} />
   </React.StrictMode>
 )
